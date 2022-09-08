97e6a615c3e7483d035f8b157014462e264fbeec
bps78 <90796928+bps78@users.noreply.github.com>
2022-09-08 11:04:06 -0500

    Added Prog Bar

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int curProgress;
+    ProgressBar progressBar;
+    int compCount = 0;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        curProgress = 0;
+        progressBar = findViewById(R.id.progressBar2);
+        textView = findViewById(R.id.textView);
+
     }
+
+    public void buttonPress(View view){
+        curProgress += (int)(Math.random() * 8) + 3;
+        progressBar.setProgress(curProgress);
+        if(progressBar.getProgress() >= 100){
+            compCount++;
+            progressBar.setProgress(0);
+            curProgress = 0;
+            textView.setText(Integer.toString(compCount));
+        }
+    }
+
+
+
 }
